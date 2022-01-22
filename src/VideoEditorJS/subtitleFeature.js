412c25b051a8659e30459b74f3bb09bc68acfc16
Karan Singh Manral <66790855+KaranManral@users.noreply.github.com>
2022-01-22 14:46:07 +0530

    Update subtitleFeature.js

--- src/VideoEditorJS/subtitleFeature.js
+++ src/VideoEditorJS/subtitleFeature.js
@@ -158,7 +158,7 @@ const get_video_source_from_input = async (input) => {
     async function () {
       try{
         ActualSourceurl = reader.result
-        fetch_and_load_Video_to_FFmpeg()
+        await fetch_and_load_Video_to_FFmpeg()
       }catch(e)
       {
         document.getElementById("ErrorBoxMessage").innerHTML="<center><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#ff0000' class='bi bi-exclamation-triangle' viewBox='0 0 16 16'><path d='M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z'/><path d='M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z'/></svg><br><b>Your file couldn't be processed on this browser. Please try this on latest version of Google Chrome Desktop.</b></center>";
@@ -200,7 +200,7 @@ const UploadSubs = async (input) => {
     async function () {
       try{
         SubSourceurl = reader.result;
-        fetch_and_load_Subs_to_FFmpeg();
+        await fetch_and_load_Subs_to_FFmpeg();
       }catch(e)
       {
         document.getElementById("ErrorBoxMessage").innerHTML="<center><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#ff0000' class='bi bi-exclamation-triangle' viewBox='0 0 16 16'><path d='M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z'/><path d='M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z'/></svg><br><b>Your file couldn't be processed on this browser. Please try this on latest version of Google Chrome Desktop.</b></center>";
@@ -554,7 +554,6 @@ function exportSRT() {
     document.getElementById("EditBox").style.display = 'none'
     Landing.style.display = 'inherit'
     CancelProcess.style.display = 'inherit'
-    console.log;
     let tempString="";
     for(let i=1;i<startTime.length;i++)
     {
